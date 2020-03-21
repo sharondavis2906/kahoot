e59676e9d69b735da60ba08338a7e4bc26991298
sharondavis2906 <57215509+sharondavis2906@users.noreply.github.com>
2020-03-21 22:26:56 +0200

    Add files via upload

--- kahoot.py
+++ kahoot.py
@@ -1,7 +1,8 @@
 # Kahoot server
 
-from flask import Flask, render_template, redirect, url_for
-from flask_socketio import SocketIO, send, emit
+from flask import Flask, render_template, redirect, url_for, request
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretkey'
@@ -12,8 +13,15 @@ pinname = '1234'
 gamename = ''
 numplayers = 0
 
-players = ['','','','','']
+question_num = 1
+question_time = 30
 
+
+lines = []
+
+players = ['','','','','']
+playersid = ['','','','','']
+scores = [0,0,0,0,0]
 
 
 @app.route('/')
@@ -21,22 +29,24 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/join')
-def join():
-    return render_template('join.html')
+
+
+@app.route('/player')
+def player():
+    return render_template('player.html')
+
+
 
 
 @app.route('/question')
 def question():
     return render_template('question.html')
 
-@app.route('/wait')
-def wait():
-    return render_template('wait.html')
 
-@app.route('/nickname')
-def nickname():
-    return render_template('nickname.html')
+
+@app.route('/answer')
+def answer():
+    return render_template('answer.html')
 
 
 @app.route('/start')
@@ -53,8 +63,8 @@ def receive_message_from_user_start(message):
     global players
     numplayers = 0
 
-    for x in range (0,4):
-      players[x]=''
+    for p in range (0,4):
+      players[p]=''
 
       
     emit('redirect from flask to start', {'url': url_for('index')})
@@ -68,11 +78,58 @@ def receive_message_from_user_start(message):
      
     global gamename 
     global pinname
+    global question_num
+    global question_time
     
-    emit('redirect from flask to start', {'url': url_for('question',game=gamename,pin=pinname)})
+    question_time = 5
+    
+    # Display question on host screen
+    emit('redirect from flask to question', {'url': url_for('question',game=gamename,pin=pinname,que_num=question_num, que_time=question_time)}, namespace='/question')
+
+    # Answer screen on client screen
+    #p=0
+    #while p < numplayers:
+      #emit('redirect from flask to question', {'url': url_for('answer',game=gamename,pin=pinname,que_num=question_num, que_time=question_time)}, namespace='/answer')
 
 
+# send question time
 
+
+# Question timeout
+@socketio.on('question timeout', namespace='/question')
+def receive_message_from_user(message):
+    print('USER MESSAGE FROM QUESTION TIMEOUT: {}'.format(message))
+ 
+
+    global gamename 
+    global pinname
+    global question_num
+    global question_time
+
+    # Redirect to results
+     
+       
+# timeout before question is displayed      
+@socketio.on('question timeoutp', namespace='/question')
+def receive_message_from_user(message):
+    print('USER MESSAGE FROM PRE-QUESTION TIMEOUT: {}'.format(message))
+ 
+
+    global gamename 
+    global pinname
+    global question_num
+    global question_time
+    global lines
+
+    linen = (question_num-1) * 5
+
+    print('LINES: {}'.format(lines))
+
+    # Display the question on Host
+    emit('message from flask to question',{'question':lines[linen],'answer1':lines[linen+1],'answer2':lines[linen+2],'answer3':lines[linen+3],'answer4':lines[linen+4]},namespace='/question')    
+
+    # Display the answer on Player
+      
 
 # Message from HTML/JS after selecting a game 
 @socketio.on('game selected', namespace='/')
@@ -80,7 +137,18 @@ def receive_message_from_user(message):
     print('USER MESSAGE FROM INDEX: {}'.format(message))
     
     global gamename
+    global lines
     gamename = message
+    
+    # Load file
+    
+    fname = gamename + ".txt"
+    
+    filep = open(fname, "r")
+    lines = filep.readlines()
+    filep.close()
+    
+    print('LINESXXX: {}'.format(lines))
     
     #emit('from flask', message.upper())
  
@@ -96,26 +164,32 @@ def receive_message_from_user(message):
     global pinname
     global numplayers
     
-    emit('redirect from flask to index', {'url': url_for('start',game=gamename,players=numplayers,pin=pinname,player1=players[0],player2=players[1],player3=players[2],player4=players[3],player5=players[4])})
-    #emit('redirect from flask', {'url': uu})
-
-
-# Message from HTML/JS after entering PIN in Join 
-@socketio.on('gamepin', namespace='/join')
+    numplayers = 0
+    
+    
+    emit('redirect from flask to index', {'url': url_for('start',game=gamename,pin=pinname)})
+    
+   
+   
+   
+   
+# Message from HTML/JS after a player connects and enters the PIN
+@socketio.on('gamepin', namespace='/player')
 def receive_message_from_user_join(message):
-    print('USER MESSAGE PIN ENTERED FROM JOIN: {}'.format(message))
+    print('USER MESSAGE PIN ENTERED FROM PLAYER: {}'.format(message))
 
     global pinname
     
     if message == pinname:
-      emit('redirect from flask', {'url': url_for('nickname')})
+      emit('message from flask to player', {'mode': 'pinok'})
     else:
-      emit('redirect from flask', {'url': url_for('join',status='error')})
-
-
-
-# Message from HTML/JS after entering NICKNAME in Nickname 
-@socketio.on('nickname', namespace='/nickname')
+      emit('message from flask to player', {'mode': 'badpin'})
+   
+   
+   
+   
+   # Message from HTML/JS after entering the nickname 
+@socketio.on('nickname', namespace='/player')
 def receive_message_from_user_join(message):
     print('USER MESSAGE PIN ENTERED FROM NICKNAME: {}'.format(message))
 
@@ -126,31 +200,30 @@ def receive_message_from_user_join(message):
 
     if message == '':
       # Error
-      emit('redirect from flask', {'url': url_for('nickname',status='error')})
+      emit('message from flask to player', {'mode': 'badname'})
     else:     
     
       players[numplayers] = message     # Name of player
+      playersid[numplayers] = request.sid
       numplayers += 1
-      #emit('redirect from flask', {'url': url_for('active')})
+                 
+      # Host
+      emit('message from flask to start',{'numplayers':numplayers,'player1':players[0],'player2':players[1],'player3':players[2],'player4':players[3],'player5':players[4]},broadcast=True,include_self=False,namespace='/start')
       
-      # Broadcast not needed in this implementation (will be required if multiple cuncurrent games are supported)
-      emit('redirect from flask to start', {'url': url_for('start',game=gamename,players=numplayers,pin=pinname,player1=players[0],player2=players[1],player3=players[2],player4=players[3],player5=players[4])},broadcast=True,include_self=False,namespace='/start')
+      # Players
+      emit('message from flask to player', {'mode': 'nameok'})
 
-      emit('redirect from flask', {'url': url_for('wait',type='game',time='')})
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 
-
-'''
-@socketio.on('message')
-def receive_message(message):
-    print('########: {}'.format(message))
-    send('This is a message from Flask.')
-
-@socketio.on('custom event')
-def receive_custom_event(message):
-    print('THE CUSTOM MESSAGE IS: {}'.format(message['name']))
-    emit('from flask', {'extension' : 'Flask-SocketIO'}, json=True)
-
-'''
 
 if __name__ == '__main__':
     socketio.run(app)
